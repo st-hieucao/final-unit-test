5ef5b6c50840338bacf2ea98a50730b19a6c38ee
CaoKhaHieu <caokhahieu9@gmail.com>
2023-04-23 17:11:56 +0700

    handle user action

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import UserList from './pages/user-list';
+import UserDetail from './pages/user-detail';
 
 function App() {
   return (
     <>
-      app
+      <Routes>
+        <Route path='/' element={<UserList />} />
+        <Route path='/user/:id' element={<UserDetail />} />
+      </Routes>
     </>
   );
 }
